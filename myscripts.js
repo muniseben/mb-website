0f6ccabaf39fb5a0a70049510418496df115ae67
Munise Ben <munise0704@gmail.com>
2022-12-16 16:55:11 +0100

    second changes added

--- myscripts.js
+++ myscripts.js
@@ -1,37 +1,33 @@
 var swiper = new Swiper(".mySwiper", {
-
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
 
     navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-
 });
 
 //! Scroll to top button
-const scrollTopButton = document.querySelector('.scrollToTop-btn');
+const scrollTopButton = document.querySelector(".scrollToTop-btn");
 
-window.addEventListener("scroll", function(){
-    scrollTopButton.classList.toggle("active",window.scrollY > 500);
+window.addEventListener("scroll", function () {
+    scrollTopButton.classList.toggle("active", window.scrollY > 500);
 });
 
-scrollTopButton.addEventListener("click",() =>{
+scrollTopButton.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
 
 //! add scroll and online
-
-
 
 //! Portfolio Section - dokumanimizdan 3 adet element sectik
 const portfolioModels = document.querySelectorAll(".portfolio-model");
@@ -39,25 +35,69 @@ const imgCards = document.querySelectorAll(".img-card");
 const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
 
 //! portfolioIn fonks olusturdum. tiklandiginda imgCard elementine active class'ini ekliyor. boylece gorunur oluyor.
-var portfolioIn = function(index){
+var portfolioIn = function (index) {
     portfolioModels[index].classList.add("active");
-}
+};
 
 //! forEach(1value, 2index, 3array)metodu kullanar eslesme yaptik.
-imgCards.forEach((imgCard, i)=>{
+imgCards.forEach((imgCard, i) => {
     // imgCards[i]=imgCard - bu ikisi de ayni anlma gelir.
     imgCard.addEventListener("click", () => {
         portfolioIn(i);
-    })
-})
+    });
+});
 
 portfolioCloseBtns.forEach((portfolioCloseBtn) => {
-    portfolioCloseBtn.addEventListener("click", ()=>{
-        portfolioModels.forEach((portfolioModalView)=>{
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModels.forEach((portfolioModalView) => {
             portfolioModalView.classList.remove("active");
         });
     });
-})
+});
+
+//! Responsive navigation menu toggle
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+        navigation.classList.remove("active");
+    });
+});
+
+//! Scroll reveal animations
+//!common reveal options to create reveal animations
+
+ScrollReveal({
+    reset: true,
+    distance: "60px",
+    duration: 2500,
+    delay: 100,
+});
+
+//!target elements, and specify options to create reveal animations
+
+ScrollReveal().reveal(".home .info h2, .section-title-01, .section-title-02", { delay: 400, origin: "left" });
+ScrollReveal().reveal(".home .info h3, .home .info p, about-info btn", { delay: 400, origin: "right" });
+ScrollReveal().reveal(".home .info .btn", { delay: 700, origin: "bottom" });
+ScrollReveal().reveal(".media-icons i, .contact-left li ", { delay: 500, origin: "left", interval: 200 });
+ScrollReveal().reveal(".home-img, .about.img", { delay: 500, origin: "bottom" });
+ScrollReveal().reveal(".about .description, .contact-right", { delay: 600, origin: "right" });
+ScrollReveal().reveal(".about .professional-list li", { delay: 500, origin: "right", interval: 200 });
+ScrollReveal().reveal(".skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2", { delay: 700, origin: "left" });
+ScrollReveal().reveal(".experience-card, .service-card, .education, .portfolio .img-card", { delay: 800, origin: "bottom", interval: 200 });
+ScrollReveal().reveal(".footer, .group", { delay: 500, origin: "top", interval: 200 });
 
 // Navigation bar effects on scroll
 // window.addEventListener("scroll", function(){
@@ -87,4 +127,3 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 //         });
 //     });
 // })
-
