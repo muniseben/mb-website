73bd69032edab9426a721a2df1c17ea2e6c6736a
Munise Ben <munise0704@gmail.com>
2022-12-16 19:57:12 +0100

    animation added

--- myscripts.js
+++ myscripts.js
@@ -56,7 +56,6 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 });
 
 //! Responsive navigation menu toggle
-
 const menuBtn = document.querySelector(".nav-menu-btn");
 const closeBtn = document.querySelector(".nav-close-btn");
 const navigation = document.querySelector(".navigation");
@@ -78,7 +77,6 @@ navItems.forEach((navItem) => {
 
 //! Scroll reveal animations
 //!common reveal options to create reveal animations
-
 ScrollReveal({
     reset: true,
     distance: "60px",
@@ -87,17 +85,16 @@ ScrollReveal({
 });
 
 //!target elements, and specify options to create reveal animations
-
-ScrollReveal().reveal(".home .info h2, .section-title-01, .section-title-02", { delay: 400, origin: "left" });
-ScrollReveal().reveal(".home .info h3, .home .info p, about-info btn", { delay: 400, origin: "right" });
-ScrollReveal().reveal(".home .info .btn", { delay: 700, origin: "bottom" });
-ScrollReveal().reveal(".media-icons i, .contact-left li ", { delay: 500, origin: "left", interval: 200 });
-ScrollReveal().reveal(".home-img, .about.img", { delay: 500, origin: "bottom" });
-ScrollReveal().reveal(".about .description, .contact-right", { delay: 600, origin: "right" });
-ScrollReveal().reveal(".about .professional-list li", { delay: 500, origin: "right", interval: 200 });
-ScrollReveal().reveal(".skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2", { delay: 700, origin: "left" });
-ScrollReveal().reveal(".experience-card, .service-card, .education, .portfolio .img-card", { delay: 800, origin: "bottom", interval: 200 });
-ScrollReveal().reveal(".footer, .group", { delay: 500, origin: "top", interval: 200 });
+ScrollReveal().reveal(".home .info h2, .section-title-01, .section-title-02", { delay: 150, origin: "left" });
+ScrollReveal().reveal(".home .info h3, .home .info p, about-info btn", { delay: 150, origin: "right" });
+ScrollReveal().reveal(".home .info .btn", { delay: 150, origin: "bottom" });
+ScrollReveal().reveal(".media-icons i, .contact-left li ", { delay: 150, origin: "left", interval: 150 });
+ScrollReveal().reveal(".home-img, .about.img", { delay: 150, origin: "bottom" });
+ScrollReveal().reveal(".about .description, .contact-right", { delay: 150, origin: "right" });
+ScrollReveal().reveal(".about .professional-list li", { delay: 150, origin: "right", interval: 150 });
+ScrollReveal().reveal(".skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2", { delay: 150, origin: "left" });
+ScrollReveal().reveal(".experience-card, .service-card, .education, .portfolio .img-card", { delay: 150, origin: "right", interval: 150 });
+ScrollReveal().reveal(".footer, .group", { delay: 150, origin: "top", interval: 150 });
 
 // Navigation bar effects on scroll
 // window.addEventListener("scroll", function(){
